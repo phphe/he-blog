72db11805663a5356910928a718255d0bf7b1d78
phphe <phphe@outlook.com>
2023-02-28 00:26:51 +0800

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 进入生成的文件夹
+cd dist
+# custom domain
+echo "phphe.com" > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+git commit -m 'rebuild pages' --allow-empty
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:phphe/he-blog.git master:gh-pages
+
+rm -rf .git
+cd -
